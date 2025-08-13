5b7d24f1c41d73e2e421aae7eefb89afa9698dec
vianaIgor <igoorviannax@gmail.com>
2025-08-12 22:16:21 -0300

    Fetches live currency rates for conversion
    Replaces hardcoded exchange rates with real-time values
    from an external API to ensure accurate currency conversion.
    Improves reliability and keeps conversion logic up to date.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
-function convertValues(){
+const convertValues = async () => {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")// Valor real
     const currencyValueConverted = document.querySelector(".currency-value")
     
-    const euroToday = 6.43
-    const dolarToday = 5.48
+const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const euroToday = data.EURBRL.high
+    const dolarToday = data.USDBRL.high
     const libraToday = 7.48
-    const bitcoinToday = 594061.43
+    const bitcoinToday = data.BTCBRL.high
     const rupeeToday = 0.064
 
     if(currencySelect.value == "dolar"){
